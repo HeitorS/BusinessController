e566650e4b7e1e22833bf737fe65e809cc8a1e28
Heitor Victor <heitor.victor96@outlook.com.br>
2018-11-04 16:24:32 -0200

    Commit de todas as alterações feitas durante a semana

---
+++ BusinessController/BusinessController/DAO/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BusinessController.DAO
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("name=Matriz") { }
+
+    }
+}
---
+++ BusinessController/BusinessController/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BusinessController.Models;
+using System.Linq;
+using System.Web;
+using System;
+
+namespace BusinessController.Repositories
+{
+    public class UserRepository : Repository<MTUSUARIO>
+    {
+    }
+}
